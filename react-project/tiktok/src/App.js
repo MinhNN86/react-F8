36a3c338da570bbfb9094cec8b7f0d0c76730cc3
MinhNN <minhnguyennhatt@gmail.com>
2025-07-06 23:28:21 +0700

    react

--- react-project/tiktok/src/App.js
+++ react-project/tiktok/src/App.js
@@ -1,32 +1,7 @@
-import { useState } from "react";
-
 function App() {
-  const jobData = JSON.parse(localStorage.getItem("jobData"));
-
-  const [job, setJob] = useState("");
-  const [jobs, setJobs] = useState(jobData ?? []);
-
-  const handleSubmit = () => {
-    setJobs((prev) => {
-      const newJob = [...prev, job];
-
-      // save job
-      localStorage.setItem("jobData", JSON.stringify(newJob));
-      return newJob;
-    });
-    setJob("");
-  };
-
   return (
-    <div style={{ padding: 30 }}>
-      <input value={job} onChange={(e) => setJob(e.target.value)} />
-      <button onClick={handleSubmit}>Add</button>
-
-      <ul>
-        {jobs.map((job, index) => (
-          <li key={index}>{job}</li>
-        ))}
-      </ul>
+    <div style={{ padding: 20 }}>
+      <h1>Test</h1>
     </div>
   );
 }
